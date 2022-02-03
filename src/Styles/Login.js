10ae7fc48ea8ed5786b166cbe5d75ea4a68ec029
macbenites <n00040723@upn.pe>
2022-02-03 16:43:09 -0500

    Add LaningPage / create component Button

--- src/Styles/Login.js
+++ src/Styles/Login.js
@@ -1,70 +1,69 @@
 import styled from "styled-components";
 
-export const LoginDiv = styled.div`    
-    form{
-        display: flex;
-        justify-content: center;        
-    }
+export const LoginDiv = styled.div`
+  form {
+    display: flex;
+    justify-content: center;
+  }
 
-    h3{
-        font-weight: 800;
-        font-size: 3rem;
-        font-family: Roboto;
-        font-style: normal;
-        margin-bottom: 7.9rem;
-        margin-top: 20rem;
-        text-align: center;
-        color: #E8E4E6;        
-    }
+  h3 {
+    font-weight: 800;
+    font-size: 3rem;
+    font-family: Roboto;
+    font-style: normal;
+    margin-bottom: 7.9rem;
+    margin-bottom: 5rem;
+    text-align: center;
+    color: #e8e4e6;
+  }
 
-    p{
-        color: #FFFFFF;
-    }
-`
-
+  p {
+    color: #ffffff;
+  }
+`;
 
 export const InputDiv1 = styled.div`
-    width: 21.9rem;
-    height: 3.8rem;
-    margin-bottom: 1rem;
-    margin-right: 11.5rem;
-    input{
-        width: 100%;
-        height: 100%;
-        background-color: #206F6C;
-        color: #FFFFFF;
-        text-align: left;
-        border-radius: 0.6rem;
-        border: none;        
-    }
-`
+  width: 21.9rem;
+  height: 3.8rem;
+  margin-bottom: 1rem;
+  margin-right: 11.5rem;
+  input {
+    width: 100%;
+    height: 100%;
+    background-color: #206f6c;
+    color: #ffffff;
+    text-align: left;
+    border-radius: 0.6rem;
+    border: none;
+  }
+`;
 
 export const BtnDiv = styled.div`
-    width: 21.9rem;
-    height: 3.8rem;
-    margin-bottom: 1rem;
-    button{
-        width: 100%;
-        height: 100%;
-        background-color: #F9BC60;
-        color: black;
-        border-radius: 0.6rem;
-        border: none;
-        text-align: center;
-    }
-`
+  width: 21.9rem;
+  height: 3.8rem;
+  margin-bottom: 1rem;
+  button {
+    width: 100%;
+    height: 100%;
+    background-color: #f9bc60;
+    color: black;
+    border-radius: 0.6rem;
+    border: none;
+    text-align: center;
+  }
+`;
 
 export const InputDiv2 = styled.div`
-    width: 21.9rem;
-    height: 3.8rem;
-    margin-bottom: 1rem;
-    input{
-        width: 100%;
-        height: 100%;
-        background-color: #004643;
-        color: #FFFFFF;
-        text-align: left;
-        border-radius: 0.6rem;
-        border: 1px solid #FFFFFF;        
-    }
-`
+  width: 21.9rem;
+  height: 3.8rem;
+  margin-bottom: 1rem;
+  input {
+    width: 100%;
+    height: 100%;
+    background-color: #004643;
+    color: #ffffff;
+    text-align: left;
+    border-radius: 0.6rem;
+    border: 1px solid #ffffff;
+  }
+`;
