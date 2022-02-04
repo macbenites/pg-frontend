8606cefd77c15b91012bef39c9af262bac210759
Yeison Rodriguez <rodriguezyj.rodriguez@gmail.com>
2022-02-04 14:43:27 -0300

    Iconos parcial Login

---
+++ src/Components/SocialListLogin.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { IconList } from "../Styles/SocialList";
+import { socialMediaLogin } from "../Utils/socialMediaLogin";
+
+function SocialListLogin() {
+  return (
+    <div>
+      {socialMediaLogin.map((item, index) => (
+        <IconList href={item.url} key={index} target="_blank" rel="noreferrer">
+          {item.icon}
+        </IconList>
+      ))}
+    </div>
+  );
+}
+
+export default SocialListLogin;
