165a9168503e219444b3f31aaf3566277cd130ff
macbenites <n00040723@upn.pe>
2022-02-04 19:33:29 -0500

    Refactor component Login / add responsive

--- src/Styles/Login.js
+++ src/Styles/Login.js
@@ -1,80 +1,67 @@
 import styled from "styled-components";
+import GoogleLogin from "react-google-login";
+import { Button } from "../Styles/reusable/Button";
+
+export const BtnGoogle = styled(GoogleLogin)`
+  grid-area: google;
+  background-color: transparent;
+  border-radius: 1rem;
+`;
 
 export const LoginDiv = styled.div`
-  form {
-    display: flex;
-    justify-content: center;
-  }
+  padding: 0 2rem;
 
-  h3 {
-    font-weight: 800;
+  h4 {
+    text-align: center;
+    font-weight: 700;
+    letter-spacing: -0.05em;
     font-size: 3rem;
-    font-family: Roboto;
-    font-style: normal;
-    margin-bottom: 7.9rem;
-    margin-bottom: 5rem;
-    text-align: center;
-    color: #e8e4e6;
+    margin: 2rem 0;
+    color: var(--text-color);
   }
 
-  span {
-    color: var(--text-color);
+  form {
+    display: grid;
+    grid-gap: 0.4rem;
+    grid-template-columns: 1fr;
+    grid-template-areas:
+      "username"
+      "password"
+      "btnlogin"
+      "icon"
+      "google"
+      "google"
+      "google"
+      ;
+    grid-template-rows: auto;
+  }
+  div {
     display: flex;
     justify-content: center;
     align-items: center;
+    grid-area: icon;
     font-size: 2rem;
+    font-weight: 700;
+    padding: 1rem 0;
+    color : var(--text-color);
+  }
+
+  @media (min-width: 568px) {
+    margin: 0 auto;
+    max-width: 500px;
+    form {
+      grid-template-columns: 1fr 20% 1fr;
+      grid-template-rows: auto;
+      grid-template-areas:
+      "username icon google"
+      "password icon google"
+      "btnlogin icon google"
+      ;
+    }
   }
 `;
 
-export const InputDiv1 = styled.div`
-  width: 21.9rem;
-  height: 3.8rem;
-  margin-bottom: 1rem;
-  margin-right: 11.5rem;
-  input {
-    width: 100%;
-    height: 100%;
-    background-color: #206f6c;
-    color: var(--text-color);
-    text-align: left;
-    border-radius: 0.6rem;
-    border: none;
-  }
-`;
-
-export const BtnDiv = styled.div`
-  width: 21.9rem;
-  height: 3.8rem;
-  margin-bottom: 1rem;
-  button {
-    width: 100%;
-    height: 100%;
-    background-color: var(--Links);
-    color: black;
-    border-radius: 0.6rem;
-    border: none;
-    text-align: center;
-  }
-`;
-
-export const BtnNetworks = styled.div`
-  width: 21.9rem;
-  height: 3.8rem;
-  margin-bottom: 1rem;
-  button {
-    width: 100%;
-    height: 100%;
-    background-color: var(--primary);
-    color: var(--text-color);
-    border-radius: 0.6rem;
-    border: 2px solid white;
-    text-align: center;
-
-  }
-
-  span{    
-    justify-content: left;
-    align-items: center;
-    //margin-top: 0.3rem;
-  }
+export const BtnLogIn = styled(Button)`
+ grid-area : btnlogin;
+  width: 100%;
 `;
