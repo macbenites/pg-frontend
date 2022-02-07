bf3bddc96acb50dbbdb232975bf014d7964a67b9
macbenites <n00040723@upn.pe>
2022-02-07 11:59:03 -0500

    Modify component Cards // add file utils/infoCard.js

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import { MainCards } from "../Styles/Cards";
+import { MainCards, Amount, Description } from "../Styles/Cards";
 
-export default function Cards({ image, title, number }){
-    return(
-        <MainCards>
-        <main>
-            <img src='pngwing.com.png' alt='pngwing.com.png' width='50px' height='50px'/>
-            <h4>{title}Canchas dispnibles</h4>
-            <h2>{number} 10</h2>
-        </main>
-        </MainCards>
-    );
-};
+export default function Cards({ name, description, link, amount }) {
+  return (
+    <MainCards>
+      <div>
+        <img src={link} alt={name} />
+      </div>
+      <Description>{description}</Description>
+      <Amount>{amount}</Amount>
+    </MainCards>
+  );
+}
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,41 +1,36 @@
-import React from "react"
-import Logo from "./Logo"
-import NavBar from "./NavBar"
-import Cards from "./Cards"
-import { HomeStyle , IntroStyle, CardsStyle } from "../Styles/Home"
+import React from "react";
+import Logo from "./Logo";
+import NavBar from "./NavBar";
+import Cards from "./Cards";
+import { infoCard } from "../Utils/infoCards";
+import { HomeStyle, IntroStyle, CardsStyle } from "../Styles/Home";
 
-function Home () {
-    return (
+function Home() {
+  return (
+    <div>
+      <HomeStyle>
+        <Logo />
+        <NavBar />
+      </HomeStyle>
+      <IntroStyle>
         <div>
-            <HomeStyle>
-                <Logo/>
-                <NavBar/>
-            </HomeStyle>
-            <IntroStyle>
-                <div>
-                    <h4>Hola Yeison</h4>
-                    <p>Bienvenido</p>
-                </div>
-                <input type="text" placeholder="Buscar..."/>
-            </IntroStyle>
-            <CardsStyle>
-                <div>
-                <Cards/> 
-                </div>
-                <div>
-                <Cards/>
-                </div>
-                <div>
-                <Cards/>
-                </div>
-                <div>
-                <Cards/>
-                </div>
-                <div>
-                <Cards/>
-                </div>
-            </CardsStyle>
+          <h4>Hola Yeison</h4>
+          <p>Bienvenido</p>
         </div>
-    )
+        <input type="text" placeholder="Buscar..." />
+      </IntroStyle>
+      <CardsStyle>
+        {infoCard.map((card) => (
+          <Cards
+            key={card.name}
+            name={card.name}
+            link={card.link}
+            description={card.description}
+            amount={card.amount}
+          />
+        ))}
+      </CardsStyle>
+    </div>
+  );
 }
-export default Home
+export default Home;
