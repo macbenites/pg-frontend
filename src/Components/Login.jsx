d9f80ad2ccdd58fc40f2748f6efe9705e357ea75
Elbruce4 <78322819+Elbruce4@users.noreply.github.com>
2022-02-08 14:34:48 -0300

    inicio sesión facebook y google conectado a firebase

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -32,7 +32,7 @@ export default function Login() {
   // credencial google
   //658093002098339 -- creedencial Facebook
 
-  const {logIn} = useContext(context);
+  const {logIn , logInWithGoogle , logInWithFacebook} = useContext(context);
 
   const onSubmit = async (event) => {
     console.log(event);
@@ -45,18 +45,18 @@ export default function Login() {
     }
   };
 
-  const responseGoogle = (response) => {
-    console.log(response);
-    console.log(response.profileObj);
-  };
+  const googleLogIn = async () => {
+    const googleData = await logInWithGoogle();
+    console.log(googleData)
+    navigate('/home');
+  }
 
-  const responseFacebook = (response) => {
-    console.log(response);
-  };
+  const facebookLogIn = async () => {
+    const facebookData = await logInWithFacebook();
+    console.log(facebookData)
+    navigate('/home');
+  }
 
-  const componentClicked = () => {
-    console.log("logueado");
-  };
   console.log(errors);
   return (
     <div>
@@ -107,26 +107,8 @@ export default function Login() {
           </ContainerInput>
           <Bar>/</Bar>
           <ContainerInput>
-            <BtnGoogle>
-              <GoogleLogin
-                clientId="742910100228-bf1uk5tddd041i1fvd8bjcg77fk7qkmo.apps.googleusercontent.com"
-                buttonText="Ingresar con Google"
-                onSuccess={responseGoogle}
-                onFailure={responseGoogle}
-                cookiePolicy={"single_host_origin"}
-              />
-            </BtnGoogle>
-            <BtnFacebook>
-              <FacebookLogin
-                appId="658093002098339"
-                autoLoad={false}
-                fields="name,email,picture"
-                onClick={componentClicked}
-                callback={responseFacebook}
-                icon="fa-facebook"
-                textButton="Ingresa con facebook"
-              />
-            </BtnFacebook>
+            <BtnGoogle><button onClick={googleLogIn}>Google LogIn</button></BtnGoogle>
+            <BtnFacebook onClick={facebookLogIn}><button>Facebook LogIn</button></BtnFacebook>
             <BtnApple type="button">Apple</BtnApple>
           </ContainerInput>
         </form>
