63fcd77f3de91c3e0f01e22a7344af8700d303fe
Yeison Rodriguez <rodriguezyj.rodriguez@gmail.com>
2022-02-12 05:50:00 -0300

    Validacion Formulario GamesCreate y DateTimePicker Parcial

---
+++ src/Styles/GamesCreate.js
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+import { LinkTo } from "../Styles/reusable/LinkTo";
+import { Button } from "../Styles/reusable/Button";
+
+
+export const CreateDiv = styled.div`
+  padding: 0 2rem;
+  input {
+    margin: 0.2rem 0;
+  }
+
+  h4 {
+    font-weight: 700;
+    letter-spacing: -0.05em;
+    font-size: 3rem;
+    margin: 2rem 0;
+    color: var(--text-color);
+  }
+
+  h5 {
+    font-weight: 200;
+    font-size: 1rem;
+    font-style: normal;
+    color: #e8e4e6;
+  }
+
+  @media (min-width: 568px) {
+    margin: 0 auto;
+    max-width: 500px;
+    form {
+      display: grid;
+      grid-gap: 0.5rem;
+      grid-template-columns: 1fr 1fr;
+      grid-template-rows: auto;
+      grid-template-areas: "name user" "barrio position" "email email" "password password" "btn btn";
+    }
+  }
+`;
+
+export const BtnCreateGame = styled(Button)`
+  width: 100%;
+  margin-top: 1rem;
+`;
+
+export const LinkToSignIn = styled(LinkTo)`
+  color: var(--tertiary);
+  font-weight: 600;
+`;
