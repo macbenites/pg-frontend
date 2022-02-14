ec9e1fd50da91c23ba8574c58d0d31917f68adb2
Elbruce4 <78322819+Elbruce4@users.noreply.github.com>
2022-02-13 23:45:21 -0300

    mostrando fields

--- src/Components/Fields.jsx
+++ src/Components/Fields.jsx
@@ -1,10 +1,26 @@
 import CardsCity from "./CardsCity";
-//import Map from "./Map";
+import Map from "./Map";
+import { useSelector , useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { getFields } from "../Redux/Actions";
+
 const Fields = () => {
+
+  const fields = useSelector (obj => obj.fields);
+  const tenFields = fields.slice(0,10)
+  const dispatch = useDispatch();
+
+  useEffect (()=> dispatch(getFields()) ,[])
+  console.log(fields)
+
   return (
     <div>
-      <CardsCity />
-      {/* <Map /> */}
+      {
+        tenFields && tenFields.map(obj => {
+         return <CardsCity key={obj.id} props={obj}/>
+        })
+      }
+      <Map />
     </div>
   );
 };
