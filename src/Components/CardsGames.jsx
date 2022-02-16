b8da068e7b0703f96c57b21bd4285f1b140ea2bd
Juan Racca <juanracca_@hotmail.com>
2022-02-16 13:01:35 -0300

    agregando boton inivitar en /matches

--- src/Components/CardsGames.jsx
+++ src/Components/CardsGames.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import {MainCardsGames, DateStyle, Players, NameCenter, ButtonCardsGames} from '../Styles/CardsGames'
+import {MainCardsGames, DateStyle, Players, NameCenter, ButtonJoinCardsGames, BtnInviteCardsGames} from '../Styles/CardsGames'
 import { joinMatch } from "../Redux/Actions";
 
 function CardsGames({nameCenter, date, players}){
 
     const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     function handleClick(e){
         e.preventDefault();
@@ -13,15 +15,18 @@ function CardsGames({nameCenter, date, players}){
         alert('Te has unido con éxito');
     };
 
+    function handleCLickInvite(e){
+        navigate('/home/players');
+    };
+
     return (
         <div>
             <MainCardsGames>
                 <NameCenter>{nameCenter}</NameCenter>
                 <DateStyle>Fecha y hora: {date}</DateStyle>
-                <Players>Jugadores: {players}</Players>
-                <ButtonCardsGames onClick={e => handleClick(e)}>
-                    Unirme
-                </ButtonCardsGames> 
+                <Players>Faltan: {players} jugadores</Players>
+                <ButtonJoinCardsGames onClick={e => handleClick(e)}>Unirme</ButtonJoinCardsGames> 
+                <BtnInviteCardsGames onClick={e => handleCLickInvite(e)}>Invitar</BtnInviteCardsGames>
             </MainCardsGames>
         </div>
     );
