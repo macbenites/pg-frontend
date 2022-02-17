71ecb85ab15fe47db5a50b93cc36aca326f4cb43
Marlon Alexis Acosta Benites <59841113+macbenites@users.noreply.github.com>
2022-02-17 16:27:43 -0500

    Delete useSelector

--- src/Components/JoinMatch.jsx
+++ src/Components/JoinMatch.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { joinMatch } from '../Redux/Actions'
 import { useState } from 'react';
 
