50be4a1122382906b12fd45da28d059534d896ee
Juan Racca <juanracca_@hotmail.com>
2022-02-23 03:46:21 -0300

    agregando deleteMatch a YourMatchesDetail.jsx

--- src/Components/YourMatchesDetail.jsx
+++ src/Components/YourMatchesDetail.jsx
@@ -3,19 +3,29 @@ import {
     DateStyle,
     NameCenter,
     Text
-     } from "../Styles/component/CardsGames"
+} from "../Styles/component/CardsGames";
+import { deleteMatch } from '../Redux/Actions';
+import { useDispatch } from "react-redux";
 
 function YourMatchesDetail ({props}) {
 
-    console.log(props)
+    const dispatch = useDispatch();
+
+    console.log(props);
+
+    const handleDelete = (e) => {
+        e.preventDefault();
+        dispatch(deleteMatch(e.target.value));
+    };
 
     return (
         <MainCardsGames>
+            <button value={props.id_match} onClick={handleDelete}>Eliminar</button>
             <NameCenter>{props.nameCenter}</NameCenter>
             <DateStyle>{props.date}</DateStyle>
             <Text>{props.note}</Text>
         </MainCardsGames>
-    )
-}
+    );
+};
 
 export default YourMatchesDetail
